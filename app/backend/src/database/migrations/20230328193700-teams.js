45a71ee0bdfb467d1e9d15093b3a36b1235e79f2
victrosantos <victor.santos.fk@hotmail.com>
2023-03-28 18:27:53 -0300

    Feito req01

---
+++ app/backend/src/database/migrations/20230328193700-teams.js
@@ -0,0 +1,21 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("teams", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      team_name: {
+        allowNull: false,
+        type: Sequelize.STRING(255),
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable("teams");
+  },
+};
