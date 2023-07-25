7e79356d6bba2b2d69cb2ac3f96ab7653f8dd6f7
victrosantos <victor.santos.fk@hotmail.com>
2023-07-25 11:52:58 -0300

    feat: Adicionado variavel de ambiente para mudar dialect do sequelize

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -1,13 +1,19 @@
 import 'dotenv/config';
 import { Options } from 'sequelize';
 
+interface ProcessEnv {
+  DB_DIALECT?: 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql';
+}
+
+const dialectChoice: ProcessEnv['DB_DIALECT'] = process.env.DB_DIALECT as ProcessEnv['DB_DIALECT'] || 'mysql';
+
 const config: Options = {
   username: process.env.MYSQLUSER || process.env.DB_USER || 'root',
   password: process.env.MYSQLPASSWORD || process.env.DB_PASS || '123456',
   database: process.env.MYSQLDATABASE || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.MYSQLHOST || process.env.DB_HOST || 'localhost',
   port: Number(process.env.MYSQLPORT) || Number(process.env.DB_PORT) || 3002,
-  dialect: 'mysql',
+  dialect: dialectChoice,
   dialectOptions: {
     timezone: 'Z',
   },
